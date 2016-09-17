fd36539bca41dfbde750153c3cfe67dcda9cd743
Gideon Adams <gideono.adams.ga@gmail.com>
2016-09-17 11:51:59 -0500

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'gideono.adams.ga@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'gideono.adams.ga@gmail.com.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from EZ Notes landing page contact form.\n\n"."Here you go:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: gideono.adams.ga@gmail.com\n"; // We got your email please wait for 12-24hours for our response. Promo are given every week..
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
